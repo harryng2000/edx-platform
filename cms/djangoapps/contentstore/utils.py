770943ded09021e49cf7f716777ea7aca2ba9c07
Prabhanshu Pandit <prabhanshupandit.pu@gmail.com>
2019-02-13 21:44:49 +0530

    fetch LMS_BASE related to org-based site configuration

--- cms/djangoapps/contentstore/utils.py
+++ cms/djangoapps/contentstore/utils.py
@@ -22,6 +22,7 @@ from xmodule.modulestore import ModuleStoreEnum
 from xmodule.modulestore.django import modulestore
 from xmodule.modulestore.exceptions import ItemNotFoundError
 from xmodule.partitions.partitions_service import get_all_partitions_for_course
+from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
 
 log = logging.getLogger(__name__)
 
@@ -106,11 +107,7 @@ def get_lms_link_for_item(location, preview=False):
 
     # checks LMS_BASE value in site configuration for the given course_org_filter(org)
     # if not found returns settings.LMS_BASE
-    lms_base = SiteConfiguration.get_value_for_org(
-        location.org,
-        "LMS_BASE",
-        settings.LMS_BASE
-    )
+    lms_base = configuration_helpers.get_value_for_org(location.org, 'LMS_BASE', settings.LMS_BASE)
 
     if lms_base is None:
         return None
@@ -139,7 +136,7 @@ def get_lms_link_for_certificate_web_view(user_id, course_key, mode):
     assert isinstance(course_key, CourseKey)
 
     # checks LMS_BASE value in SiteConfiguration against course_org_filter if not found returns settings.LMS_BASE
-    lms_base = SiteConfiguration.get_value_for_org(course_key.org, "LMS_BASE", settings.LMS_BASE)
+    lms_base = configuration_helpers.get_value_for_org(course_key.org, 'LMS_BASE', settings.LMS_BASE)
 
     if lms_base is None:
         return None
